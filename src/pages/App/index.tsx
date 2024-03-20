b6f695a77542aad66a0463a03f4ef17d95734a3c
antonio carlos <antoniocarlosleite1992@gmail.com>
2024-03-20 16:38:10 -0300

    add project routes

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,15 +1,17 @@
+import { Outlet } from "react-router-dom";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 import { Container } from "./style";
 
 export function App() {
-   return (
+  return (
     <Container>
-      <Header/>
-      
-      <h1>App</h1>
-      
-      <Footer/>
+      <Header />
+
+      {/* outlet is main */}
+      <Outlet />
+
+      <Footer />
     </Container>
   );
 }
