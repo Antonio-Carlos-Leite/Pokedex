948d068da73202d9754a9a8e7d26ae4ad6125d47
antonio carlos <antoniocarlosleite1992@gmail.com>
2024-03-20 15:33:56 -0300

    add pages project

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./pages/App/index.tsx";
-import { Reset } from "./styles/reste.ts";
+import { Reset } from "./styles/reset.ts";
 import { ThemeProvider } from "styled-components";
 import { appTheme } from "./styles/theme.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={appTheme}>
-    <App />
-    <Reset/>
-    </ThemeProvider>
+    <QueryClientProvider client={new QueryClient()}>
+      <ThemeProvider theme={appTheme}>
+        <App />
+        <Reset />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,12 +1,15 @@
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+import { Container } from "./style";
 
 export function App() {
-
-  return (
-    <>
+   return (
+    <Container>
+      <Header/>
+      
       <h1>App</h1>
-     
-    </>
-  )
+      
+      <Footer/>
+    </Container>
+  );
 }
-
-
